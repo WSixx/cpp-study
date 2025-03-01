9e3e460e00e249850dcf0bbc01552f13914d788b
Lucas G <lucas.2007s@gmail.com>
2025-03-01 16:41:48 -0300

    test

--- ConceitoVariavel/variavel.cpp
+++ ConceitoVariavel/variavel.cpp
@@ -4,13 +4,24 @@
 int main()
 {
 	_tsetlocale(LC_ALL, _T("portuguese"));
+	const int test = 10;
 	int NumVidas = 5;
 	int Score = 1350;
-	std::cout << "***********" << std::endl;	
+	std::cout << "************GAME START**************" << std::endl;	
 	std::cout << "Vidas jogador: " << NumVidas << std::endl;
 	std::cout << "Pontuação: " << Score << std::endl;
 	std::cout << "Endereço que NumVidas ocupa na memória RAM: " << &NumVidas << "\n";
 	std::cout << "Endereço que Score ocupa na memória RAM: " << &Score << "\n";
-	std::cout << "***********" << std::endl;
+	std::cout << "Tamanho da variavel NumVidas: " << sizeof(NumVidas) << " Bytes" << "\n";
+	std::cout << "Tamanho da variavel Score:: " << sizeof(Score) << " Bytes" << "\n";
+	std::cout << "***********************************" << std::endl;
+
+	std::cout << "************IN GAMING**************" << std::endl;
+	Score = Score + 150;
+	NumVidas = NumVidas - 1;
+	std::cout << "Vidas jogador: " << NumVidas << std::endl;
+	std::cout << "Pontuação: " << Score << std::endl;
+	std::cout << "***********************************" << std::endl;
+
 	system("pause");
 }
